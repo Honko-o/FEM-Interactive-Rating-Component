cd68ea767fe2be26bda2e45e641b87c9c87eb066
Saleh <salehabuhussein2@gmail.com>
2022-04-29 06:18:50 +0300

    Finish Challenge

---
+++ main.js
@@ -0,0 +1,22 @@
+let rateButtons = document.querySelectorAll("a[data-value]");
+let rateSubmit = document.querySelector(".rating__state__submit");
+let rateValue;
+
+rateButtons.forEach(el => {
+    el.addEventListener("click", e => {
+        rateButtons.forEach(el => el.id = "");
+        e.target.id = "active";
+        rateValue = +(e.target.getAttribute("data-value"));
+    });
+});
+
+rateSubmit.addEventListener("click", el => {
+    let activeRate = document.getElementById("active");
+    if (activeRate) {
+        document.querySelector(".rating__state").classList.remove("visible");
+        document.querySelector(".rating__state").classList.add("hidden");
+        document.querySelector(".thank__you").classList.remove("hidden");
+        document.querySelector(".thank__you").classList.add("visible");
+        document.getElementById("rate-number").innerHTML = rateValue;
+    }
+})
