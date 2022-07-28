817d7c4d2c04d3941412cea336fd3d2b7272fb47
Saleh <salehabuhussein2@gmail.com>
2022-07-28 19:42:42 +0300

    Reformat HTML into Pugjs

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-let rateButtons = document.querySelectorAll("a[data-value]");
-let rateSubmit = document.querySelector(".rating__state__submit");
+let rateButtons = document.querySelectorAll('a[data-value]');
+let rateSubmit = document.querySelector('.rating__state__submit');
 let rateValue;
 
 // Rate Value Buttons Event Handlers
-rateButtons.forEach(el => {
-    el.addEventListener("click", e => {
-        rateButtons.forEach(el => el.id = "");
-        e.target.id = "active";
-        rateValue = +(e.target.getAttribute("data-value"));
+rateButtons.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+        rateButtons.forEach((el) => (el.id = ''));
+        e.target.id = 'active';
+        rateValue = +e.target.getAttribute('data-value');
     });
 });
 
 // Submit Button
-rateSubmit.addEventListener("click", el => {
-    let activeRate = document.getElementById("active");
+rateSubmit.addEventListener('click', (el) => {
+    let activeRate = document.getElementById('active');
     if (activeRate) {
-        document.querySelector(".rating__state").classList.remove("visible");
-        document.querySelector(".rating__state").classList.add("hidden");
-        document.querySelector(".thank__you").classList.remove("hidden");
-        document.querySelector(".thank__you").classList.add("visible");
-        document.getElementById("rate-number").innerHTML = rateValue;
+        document.querySelector('.rating__state').classList.remove('visible');
+        document.querySelector('.rating__state').classList.add('hidden');
+        document.querySelector('.thank__you').classList.remove('hidden');
+        document.querySelector('.thank__you').classList.add('visible');
+        document.getElementById('rate-number').innerHTML = rateValue;
     }
-})
+});
